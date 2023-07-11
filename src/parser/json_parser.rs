231338630e92c6b61092c377af72f3ca43d661f8
Yasser Tahiri <yasserth19@gmail.com>
2023-07-11 22:57:22 +0400

    ♻️ refactor `json` parser

--- src/parser/json_parser.rs
+++ src/parser/json_parser.rs
@@ -1,43 +1,34 @@
-use std::path::Path;
+use std::sync::Arc;
 
 use json::JsonValue;
 
 use crate::{
-    models::{Endpoint, Method},
-    utils::read_file_to_string_or_err,
+    config::Runtime,
+    models::{EndpointConfiguration, Method},
+    utils::Error,
 };
 
-use super::ParsingError;
+use super::common::format_basepath;
 
-pub fn parse_openapi_json(path: &Path) -> Result<Vec<Endpoint>, ParsingError> {
-    parse_json_doc(&read_file_to_string_or_err(path, ParsingError::ProblemOpeningFile)?)
-}
-
-fn parse_json_doc(json_string: &str) -> Result<Vec<Endpoint>, ParsingError> {
+pub fn parse_json_doc(json_string: &str, runtime: Arc<Runtime>) -> Result<Vec<EndpointConfiguration>, Error> {
     let mut endpoints = vec![];
 
     let json_obj = match json::parse(json_string) {
         Ok(json_obj) => json_obj,
-        Err(_) => return Err(ParsingError::InvalidSyntax),
+        Err(_) => return Err(Error::InvalidParseSyntax),
     };
 
     let base_path = match &json_obj["basePath"] {
         JsonValue::Null => "",
-        base_path => {
-            if base_path == "/" {
-                ""
-            } else {
-                println!("{}", base_path);
-                match base_path.as_str() {
-                    Some(base_path) => base_path,
-                    None => return Err(ParsingError::InvalidSyntax),
-                }
-            }
-        }
+        base_path => match base_path.as_str() {
+            Some(base_path) => base_path,
+            None => return Err(Error::InvalidBasePath),
+        },
     };
+    let base_path = format_basepath(base_path);
 
     let paths = match &json_obj["paths"] {
-        json::Null => return Err(ParsingError::InvalidSyntax),
+        json::Null => return Err(Error::InvalidParseSyntax),
         responses => responses,
     };
 
@@ -49,21 +40,44 @@ fn parse_json_doc(json_string: &str) -> Result<Vec<Endpoint>, ParsingError> {
             _ => path.push_str(path_json.0),
         }
         if path.is_empty() {
-            path.push('/');
+            path.push_str("/");
         }
 
         for (method, method_json) in get_methods_from_path(path_json.1)?.into_iter() {
             let responses = match &method_json["responses"] {
-                json::Null => return Err(ParsingError::InvalidSyntax),
+                json::Null => return Err(Error::InvalidParseSyntax),
                 responses => responses,
             };
+
+            if !&method_json["security"].is_null() {
+                endpoints.push(EndpointConfiguration::new(
+                    method.clone(),
+                    &path,
+                    401,
+                    runtime.clone(),
+                    false,
+                )?);
+                endpoints.push(EndpointConfiguration::new(
+                    method.clone(),
+                    &path,
+                    403,
+                    runtime.clone(),
+                    false,
+                )?);
+            }
 
             for response in responses.entries() {
                 let status_code = match response.0.parse() {
                     Ok(status_code) => status_code,
-                    Err(_) => return Err(ParsingError::InvalidStatusCode),
+                    Err(_) => return Err(Error::InvalidParseStatusCode(response.0.to_string())),
                 };
-                endpoints.push(Endpoint::new(method.clone(), path.clone(), status_code))
+                endpoints.push(EndpointConfiguration::new(
+                    method.clone(),
+                    &path,
+                    status_code,
+                    runtime.clone(),
+                    false,
+                )?)
             }
         }
     }
@@ -71,13 +85,13 @@ fn parse_json_doc(json_string: &str) -> Result<Vec<Endpoint>, ParsingError> {
     Ok(endpoints)
 }
 
-fn get_methods_from_path(path_json: &JsonValue) -> Result<Vec<(Method, &JsonValue)>, ParsingError> {
+fn get_methods_from_path(path_json: &JsonValue) -> Result<Vec<(Method, &JsonValue)>, Error> {
     let mut methods = vec![];
 
     for method_entry in path_json.entries() {
         let method = match Method::from_str(method_entry.0) {
             Some(method) => method,
-            None => return Err(ParsingError::InvalidMethod),
+            None => return Err(Error::InvalidParseMethod(method_entry.0.to_string())),
         };
         methods.push((method, method_entry.1));
     }
@@ -86,11 +100,13 @@ fn get_methods_from_path(path_json: &JsonValue) -> Result<Vec<(Method, &JsonValu
 
 #[cfg(test)]
 mod test {
-    use std::path::Path;
+
+    use std::{str::FromStr, sync::Arc};
 
     use crate::{
-        models::Method,
-        parser::json_parser::{parse_json_doc, parse_openapi_json},
+        models::{Method, OpenapiPath},
+        parser::json_parser::parse_json_doc,
+        utils::test::create_mock_runtime,
     };
 
     const JSON_STRING: &str = r#"
@@ -99,6 +115,7 @@ mod test {
         "paths" : {
             "/": {
                 "get": {
+                    "security": [],
                     "responses": {
                         "200": {
                             "description": "OK",
@@ -143,20 +160,25 @@ mod test {
 
     #[test]
     fn parses_correct_number_of_responses() {
-        assert_eq!(parse_json_doc(JSON_STRING).unwrap().len(), 4);
+        assert_eq!(
+            parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
+                .unwrap()
+                .len(),
+            6
+        );
     }
 
     #[test]
     fn parses_correct_status_codes() {
-        assert!(parse_json_doc(JSON_STRING)
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
             .unwrap()
             .iter()
             .any(|x| x.status_code == 200));
-        assert!(parse_json_doc(JSON_STRING)
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
             .unwrap()
             .iter()
             .any(|x| x.status_code == 400));
-        assert!(parse_json_doc(JSON_STRING)
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
             .unwrap()
             .iter()
             .any(|x| x.status_code == 418));
@@ -164,24 +186,54 @@ mod test {
 
     #[test]
     fn parses_correct_path() {
-        assert!(parse_json_doc(JSON_STRING).unwrap().iter().any(|x| x.path == "/"));
-        assert!(parse_json_doc(JSON_STRING).unwrap().iter().any(|x| x.path == "/test"));
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
+            .unwrap()
+            .iter()
+            .any(|x| x.path == OpenapiPath::from_str("/").unwrap()));
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
+            .unwrap()
+            .iter()
+            .any(|x| x.path == OpenapiPath::from_str("/test").unwrap()));
     }
 
     #[test]
     fn parses_correct_method() {
-        assert!(parse_json_doc(JSON_STRING)
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
             .unwrap()
             .iter()
             .any(|x| x.method == Method::GET));
-        assert!(parse_json_doc(JSON_STRING)
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
             .unwrap()
             .iter()
             .any(|x| x.method == Method::POST));
-        assert!(parse_json_doc(JSON_STRING)
+        assert!(parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
             .unwrap()
             .iter()
             .any(|x| x.method == Method::PUT));
+    }
+
+    #[test]
+    fn adds_401_403_for_security_headers() {
+        assert_eq!(
+            parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
+                .unwrap()
+                .iter()
+                .filter(|x| x.method == Method::GET
+                    && x.status_code == 401
+                    && x.path == OpenapiPath::from_str("/").unwrap())
+                .count(),
+            1
+        );
+        assert_eq!(
+            parse_json_doc(JSON_STRING, Arc::from(create_mock_runtime()))
+                .unwrap()
+                .iter()
+                .filter(|x| x.method == Method::GET
+                    && x.status_code == 403
+                    && x.path == OpenapiPath::from_str("/").unwrap())
+                .count(),
+            1
+        );
     }
 
     const JSON_STRING_DIFF_BASEPATH: &str = r#"
@@ -218,20 +270,17 @@ mod test {
 
     #[test]
     fn parses_correct_basepath() {
-        println!("{:?}", parse_json_doc(JSON_STRING_DIFF_BASEPATH));
-        assert!(parse_json_doc(JSON_STRING_DIFF_BASEPATH)
-            .unwrap()
-            .iter()
-            .any(|x| x.path == "/foo"));
-        assert!(parse_json_doc(JSON_STRING_DIFF_BASEPATH)
-            .unwrap()
-            .iter()
-            .any(|x| x.path == "/foo/bar"));
-    }
-
-    #[test]
-    fn parses_file_correctly() {
-        let path = Path::new("./dump/swagger.json");
-        assert_eq!(parse_openapi_json(&path).unwrap().len(), 4);
+        assert!(
+            parse_json_doc(JSON_STRING_DIFF_BASEPATH, Arc::from(create_mock_runtime()))
+                .unwrap()
+                .iter()
+                .any(|x| x.path == OpenapiPath::from_str("/foo").unwrap())
+        );
+        assert!(
+            parse_json_doc(JSON_STRING_DIFF_BASEPATH, Arc::from(create_mock_runtime()))
+                .unwrap()
+                .iter()
+                .any(|x| x.path == OpenapiPath::from_str("/foo/bar").unwrap())
+        );
     }
 }
